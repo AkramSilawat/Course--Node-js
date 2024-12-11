2c3b1ddae061a4cd030da75b61f55bdc3104898e
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2024-12-11 16:11:25 +0530

    Import Export Patterns

---
+++ 06-Import-Export-Patterns/index.js
@@ -0,0 +1,21 @@
+// const add = require("./math");
+
+// console.log(add(2,3));
+
+
+// ------------------------------------
+
+// const math = require("./math");
+
+// console.log(math.add(2,3));
+// console.log(math.subtract(2,3));
+
+
+// ------------------------------------
+
+const math = require("./math");
+
+const {add, subtract} = math;
+
+console.log(add(2,3));
+console.log(subtract(2,3));
---
+++ 06-Import-Export-Patterns/math.js
@@ -0,0 +1,29 @@
+// const add = (a, b) => {
+//     return a + b ;
+// };
+// module.exports = add;
+
+
+
+
+// module.exports = (a, b) => {
+//     return a + b ;
+// };
+
+
+
+
+exports.add = (a, b) => {
+    return a + b ;
+};
+
+exports.subtract = (a,b) => {
+    return a - b;
+};
+
+
+
+// module.exports = {
+//     add,
+//     subtract,
+// };
