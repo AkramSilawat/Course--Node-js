c8115c32366f87480fdf0ffec5f4802694f23042
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2024-12-12 12:34:55 +0530

    ES-Modules

---
+++ 08-ES-Modules/math-esm.mjs
@@ -0,0 +1,23 @@
+// const add = (a,b) => {
+//     return a + b;
+// }
+
+// export default add;
+
+
+
+
+
+export const add = (a,b) => {
+    return a + b;
+}
+
+export const subtract = (a,b) => {
+    return a - b;
+}
+
+
+// export default {
+//     add,
+//     subtract,
+// }
