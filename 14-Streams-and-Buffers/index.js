eec63a4c6ceaa39a23431fd428ae656013cd136b
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2024-12-14 22:41:20 +0530

    Streams And Buffers

---
+++ 14-Streams-and-Buffers/index.js
@@ -0,0 +1,8 @@
+const buffer = new Buffer.from("Vishwas");
+
+buffer.write("Code");
+buffer.write("Codevolution");
+
+console.log(buffer.toString())
+console.log(buffer)
+console.log(buffer.toJSON())
