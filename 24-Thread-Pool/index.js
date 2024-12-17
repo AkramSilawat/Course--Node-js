70d2427acf2ec112621e035df23c1efc16e821c1
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2024-12-17 22:38:15 +0530

    Thread Pool

---
+++ 24-Thread-Pool/index.js
@@ -0,0 +1,20 @@
+// const fs = require("node:fs");
+
+// console.log("First");
+
+// fs.readFile("./file.txt", "utf-8", (err,data) => {
+//     console.log("File contents");
+// });
+
+// console.log("Last")
+
+
+// ----------------------------------------
+
+
+// const crypto = require("node:crypto");
+
+// const start = Date.now();
+// crypto.pbkdf25ync("password", "salt", 100000, 512, "sha512");
+// console.log("Hash: ", Date.now() - start);
+
