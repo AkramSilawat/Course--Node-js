18302a6fa2febc92a24290c7d13100d0ca7fe7c8
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2024-12-19 20:49:07 +0530

    Microtast Queue

---
+++ 28-Microtast-Queues/index.js
@@ -0,0 +1,29 @@
+// console.log("console 1");
+// process.nextTick(() => console.log("this is process.next 1"))
+// console.log("console 2");
+
+//---------------------------
+
+// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+// process.nextTick(() => console.log("this is process.nextTick 1"));
+
+// --------------------------
+
+process.nextTick(() => console.log("this is process.nextTick 1"));
+process.nextTick(() => {
+    console.log("this is process.nextTick 2");
+    process.nextTick(() =>
+        console.log("this is the inner next tick inside next tick")
+    );
+});
+process.nextTick(() => console.log("this is process.nextTick 3"));
+
+Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+Promise.resolve().then(() => {
+    console.log("this is Promise.resolve 2");
+    process.nextTick(() =>
+        console.log("this is the inner next tick inside Promise then block")
+    );
+})
+
+Promise.resolve().then(() => console.log("this is Promise.resolve 3"));
